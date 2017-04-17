be6c40dfa6e26bd803d9e6ad4db1f13c4ee5780d
Pranav Kural <pranavkural042@gmail.com>
2017-04-17 13:16:44 -0400

    Import specific methods from the modules

--- Modules/app.js
+++ Modules/app.js
@@ -1,3 +1,15 @@
 // Import the default function from the calc module
 import logger from './calc';
 
+// Import specific methods from the modules
+import { add, substract } from './calc';
+
+
+// Log numbers to console
+logger(30, 80);             //First number: 30, Second number: 80
+
+// Add integers
+add(30, 50);                //80
+
+// Substract Integeres
+substract(80, 30);          //50
