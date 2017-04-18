dfc877a3d7933a51a1e8db5498600b5649d68de0
Pranav Kural <pranavkural042@gmail.com>
2017-04-18 13:52:19 -0400

    Basic ES5 calculator functions

---
+++ calculator.js
@@ -0,0 +1,28 @@
+/* Calculator functions */
+
+// Add two numbers
+function add(x, y) {
+  return x + y;
+}
+
+
+// Substract two numbers
+function substract(x, y) {
+  return x - y;
+}
+
+// Multiply two numbers
+function multiply(x, y) {
+  return x * y;
+}
+
+
+// Divide two numbers
+function divide(x, y) {
+  return x / y;
+}
+
+// Print numbers to console
+function log(x, y) {
+  console.log("First number: " + x + ", Second number: " + y);
+}
