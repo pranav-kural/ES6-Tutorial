e7c998746268cb74b0e59ff306c8313275ecb0c4
Pranav Kural <pranavkural042@gmail.com>
2017-04-18 19:39:01 -0400

    Created the calculator module and imported functions in app file

--- calculator.js
+++ calculator.js
@@ -1,16 +1,22 @@
 /* Calculator Functions */
 
 // Add two numbers
-add = (x, y) => x + y;
+export const add = (x, y) => x + y;
 
 // Substract two numbers
-substract = (x, y) => x - y;
+export const substract = (x, y) => x - y;
 
 // Multiply two numbers
 multiply = (x, y) => x * y;
 
+// Another syntax to export function
+export const multi = multiply;
+
 // Divide two numbers
 divide = (x, y) => x / y;
 
+// Yet another syntax option for exporting a function excplicitly
+export { divide };
+
 // Print numbers to console
-log = (x, y) => console.log("First number: " + x + ", Second number: " + y);
+export default log = (x, y) => console.log("First number: " + x + ", Second number: " + y);
