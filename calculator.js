101d0e10fd2c880cd8d6b0d547f696dc5fd9c044
Pranav Kural <pranavkural042@gmail.com>
2017-04-19 10:34:47 -0400

    Added new files

---
+++ calculator.js
@@ -0,0 +1,28 @@
+/* Calculator functions */
+
+// Add two numbers
+var add = function(x, y) {
+  return x + y;
+};
+
+
+// Substract two numbers
+var substract = function (x, y) {
+  return x - y;
+};
+
+// Multiply two numbers
+var multiply = function (x, y) {
+  return x * y;
+};
+
+
+// Divide two numbers
+var divide = function(x, y) {
+  return x / y;
+};
+
+// Print numbers to console
+var log = function(x, y) {
+  console.log("First number: " + x + ", Second number: " + y);
+};
