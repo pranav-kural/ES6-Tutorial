6ff43f2f2530611ec44819da2eba7ba4f3c283b3
Kyle Duffy <kyleduffy83@gmail.com>
2017-04-19 11:53:15 -0400

    added sone number validation

--- src/calculator.js
+++ src/calculator.js
@@ -4,22 +4,36 @@
 const log = (x, y) => console.log("First number: " + x + ", Second number: " + y);
 
 // Add two numbers
-const add = (x, y) => x + y;
+const add = (x, y) => validate(x) + validate(y);
 
 // Substract two numbers
-const substract = (x, y) => x - y;
+const substract = (x, y) => validate(x) - validate(y);
 
 // Multiply two numbers
-const multiply = (x, y) => x * y;
+const multiply = (x, y) =>  validate(x) * validate(y);
 
 // Divide two numbers
-const divide = (x, y) => x / y;
+const divide = (x, y) =>  validate(x) / validate(y);
 
 // Function to add all the numbers received
 const addAll = (...numbers) => numbers.reduce((x, y) => x+y);
 
 // Function to multiply all the numbers received
 const multiplyAll = (...numbers) => numbers.reduce((x, y) => x*y);
+
+const validate = (z) => {
+z = Math.trunc(z);
+if(Number.isFinite(z)
+{
+  continue;
+}
+else{
+console.log("Please enter a valid set of Number");
+break;
+}
+
+return z;}
+
 
 // Export the calculator module functions
 export { add, substract, multiply, divide, addAll, multiplyAll };
